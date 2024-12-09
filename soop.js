c6435209ecd71766577f7405d5b34cb7c2499208
jebibot <tjebibot@gmail.com>
2024-12-10 04:11:41 +0900

    feat: soop script

---
+++ soop.js
@@ -0,0 +1,22 @@
+try {
+  window.top.location.hostname;
+} catch {
+  const style = document.createElement("style");
+  style.textContent = `
+.embeded_mode #webplayer.chat_open #chatting_area {
+  display: none !important;
+}
+
+.popout_chat #chatting_area {
+  min-width: auto !important;
+}`;
+  (document.head || document.documentElement).append(style);
+
+  const params = new URLSearchParams(location.search);
+  if (params.get("vtype") === "chat") {
+    if (window.opener == null) {
+      const path = location.pathname.split("/");
+      window.opener = window.top[path[1]];
+    }
+  }
+}
